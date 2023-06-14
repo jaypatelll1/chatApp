a6612acf6abc6dbb1b8bc988353ad3f73cfe3970
Mihir <mihirvora1053@gmail.com>
2023-06-14 16:41:25 +0530

    Main Screen Complete

--- screens/Main.js
+++ screens/Main.js
@@ -18,32 +18,55 @@ export default function home() {
   };
   return (
     <View style={styles.container}>
-      {/* <View>
-      <Image source={require('../assets/main.jpg')}/>
-      </View> */}
-      <View style={styles.centerText}>
-        <Text style={{
-                color:"#4876BC",
-                fontWeight:"bold",
-                fontSize:40,
-        }}>ChatApp</Text>
+      <View style={styles.imgView}>
+        <Image
+          resizeMode={"cover"}
+          source={require("../assets/main.jpg")}
+          style={styles.mainImg}
+        />
 
-        <Text style={{
-            color:"#4876BC",
-            fontSize:12,
-            fontWeight:"bold",
-            marginTop:"2%"
-        }}>
-        Connect, Chat, and Share Moments - Your Personal Chat Hub!
+        <View
+          style={{
+            width: 80,
+            height: 80,
+            backgroundColor: "#647FDE",
+            borderRadius: 50,
+            // marginTop:"40%",
+            position: "absolute",
+            top: 300,
+            left: 135,
+          }}
+        ></View>
+      </View>
+      <View style={styles.centerText}>
+        <Text
+          style={{
+            color: "#4876BC",
+            fontWeight: "bold",
+            fontSize: 40,
+          }}
+        >
+          ChatApp
         </Text>
-      </View>  
+
+        <Text
+          style={{
+            color: "#4876BC",
+            fontSize: 12,
+            fontWeight: "bold",
+            marginTop: "2%",
+          }}
+        >
+          Connect, Chat, and Share Moments - Your Personal Chat Hub!
+        </Text>
+      </View>
       <View style={styles.buttons}>
-       <TouchableOpacity style={styles.button}>
-        <Text style={styles.text}>Login</Text>
-       </TouchableOpacity>
-       <TouchableOpacity style={styles.button}>
-        <Text style={styles.text}>Sign Up</Text>
-       </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -53,29 +76,51 @@ const styles = StyleSheet.create({
     //marginTop: 50,
     height: "100%",
   },
-  buttons:{
-    flex:1,
-    justifyContent:"flex-end",
-    alignItems:"center",
-    marginBottom:60
+  buttons: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 60,
   },
-  button:{
-    backgroundColor:"#9DB2FD",
-    padding:10,
+  button: {
+    backgroundColor: "#9DB2FD",
+    padding: 10,
 
-    width:"60%",
-    margin:12,
-    borderRadius:50
+    width: "60%",
+    margin: 12,
+    borderRadius: 50,
   },
-  text:{
-    color:"#4876BC",
-    textAlign:"center"
+  text: {
+    color: "#4876BC",
+    textAlign: "center",
   },
-  centerText:{
-    flex:1,
-    marginTop:"75%",
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center"
-  }
+  centerText: {
+    // flex: 1,
+    // marginTop: "90%",
+    // flexDirection: "column",
+    // justifyContent: "center",
+    alignItems: "center",
+    position:"absolute",
+    top:400,
+    left:18
+  },
+  mainImg: {
+    height: "90%",
+    width: "98%",
+    position: "absolute",
+    overflow: "hidden",
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    padding: 10,
+    top:26
+  },
+  imgView: {
+    backgroundColor: 'rgba(230,230,230,1)',
+    justifyContent: "center",
+    flexDirection: "row",
+    height: 320,
+    position: "relative",
+    borderBottomLeftRadius:60,
+    borderBottomRightRadius:60,
+  },
 });
