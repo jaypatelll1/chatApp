54894820bbd8af4cd83c5fcef5888eb039f838f0
jay <pateljay8886@gmail.com>
2023-06-14 15:00:47 +0530

    nhk

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet,Image } from 'react-native'
 import React from 'react'
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View>
+      <Image source={require('../assets/login.jpg')} style={styles.loginimg} />
+      
+      </View>
+      
     </View>
   )
 }
+const styles = StyleSheet.create({
+loginimg:{
+    marginTop:20,
+    height:400,
+    width: 400,
+}
+})
