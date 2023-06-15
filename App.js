8f8ba5077104bbc3da558cc0d2899512f3e6fa41
Mihir <mihirvora1053@gmail.com>
2023-06-15 12:14:56 +0530

    App.js Modified

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native';
+// import Home from './screens/Home';
+import ChatScreen from './screens/ChatScreen';
+import MainScreen from "./screens/Main"
+export default function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'User1',
+      avatar: require('./assets/avatar1.png'),
+      lastMessage: 'Hello there!',
+    },
+    {
+      id: 2,
+      username: 'User2',
+      avatar: require('./assets/avatar2.png'),
+      lastMessage: 'How are you doing?',
+    },
+    
+    
+    // Add more users as needed
+  ];
+
+  const handleUserPress = (user) => {
+    // Handle user press here
+    console.log('User pressed:', user);
+  };
+
+  return (
+    <View>
+      {/* <Home users={users} onUserPress={handleUserPress}/> */}
+      <ChatScreen/>
+    </View>
+  );
+}
+
