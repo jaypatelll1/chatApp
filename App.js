96d0ccb9c206efc1968196ccf3ce81c42e3fedcf
jay <pateljay8886@gmail.com>
2023-10-29 14:01:36 +0530

    firesbase.js added

--- App.js
+++ App.js
@@ -2,11 +2,7 @@ import { AuthProvider } from "./context/AuthContext";
 import AppNav from "./Navigation/AppNav";
 import React from "react";
 import "react-native-gesture-handler";
-import EditProfile from "./screens/EditProfile";
-import { View } from 'react-native'
-import Login from "./screens/Login";
-import Otp from "./screens/Otp";
-import DrawerNav from "./Navigation/DrawerNav";
+
 
 export default function App() {
   return (
---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnclUHC-NUNO5z3YGtqHSqPzLIUEvE2xg",
+  authDomain: "chatapp-mdj.firebaseapp.com",
+  projectId: "chatapp-mdj",
+  storageBucket: "chatapp-mdj.appspot.com",
+  messagingSenderId: "794468019489",
+  appId: "1:794468019489:web:591aeeea3ac1a7dd41e260",
+  measurementId: "G-LKDWKZBT0P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
